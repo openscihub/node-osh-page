8af76e15cfebdc52b9c99bbbc049ec0326488bd8
Carl Bauer <carl.a.bauer@gmail.com>
2014-10-17 04:14:09 -0700

    After merging with then splitting from osh-pages
    Still figuring out which logic belongs in osh-page and which in
    osh-pages.

--- example/simple.js
+++ example/simple.js
@@ -1,29 +1,18 @@
 var Page = require('..');
-var express = require('express');
-var request = require('superagent');
 var html = require('html');
-var http = require('http');
 
-var page = Page({
-  title: 'My page', 
+var MyPage = Page.extend({
   path: {
     pattern: '/home'
   },
-  body: function(props) {
-    return '<h1>Welcome!</h1>';
+  get: function(done) {
+    this.title = 'My Page';
+    this.body = '<h1>Welcome!</h1>';
   }
 });
 
-var app = express();
-page.serve(app);
-
-var server = http.createServer(app);
-server.listen(3333, function() {
-  request.get('localhost:3333/home')
-  .end(function(res) {
-    console.log(
-      html.prettyPrint(res.text)
-    );
-    server.close();
-  });
-});
+console.log(
+  html.prettyPrint(
+    MyPage().renderToString()
+  )
+);
